de3ad9a849df208f20d2a05e59310679ab6c8f20
RyoshiTheDev <michaelzambrano06@gmail.com>
2025-12-01 11:28:53 -0500

    Refactor items.js for clarity and structure

--- js/items.js
+++ js/items.js
@@ -63,11 +63,12 @@ const lootTable = {
 };
 
 const dropOdds = {
-    legendary: 0.02, // 2%
+    legendary: 0.01, // 2%
     epic: 0.10,       // 8%
     rare: 0.30,       // 20%
     common: 0.70      // 70%
 };
 
 console.log("Loaded correct ITEMS.JS");
+
 
